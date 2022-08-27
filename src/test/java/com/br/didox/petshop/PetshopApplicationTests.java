19d8ee251ee6b3d6ddf430e20005a8331ea5c0b2
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-08-27 13:37:24 -0300

    iniciando exercicio

---
+++ src/test/java/com/br/didox/petshop/PetshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.didox.petshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
